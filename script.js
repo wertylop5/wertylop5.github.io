69404bb448b04ce54a2c105d8c7b85a907624cf7
Stanley Lin <slin55055@gmail.com>
2016-01-03 13:15:37 -0500

    uhk

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function buttonClick() {
 	var button = document.getElementById("first");
-	var text = document.getElementById("hello").;
+	var text = document.getElementById("hello");
 	
 	text.innerHTML = "Clicked";
 }
